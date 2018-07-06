c5f04b01188dc3d0683aad0188edd5f6c46ffa5e
steve <steviewez@gmail.com>
2018-07-06 10:23:09 +0300

    patch - modify static root

--- ajiragis_translator/settings.py
+++ ajiragis_translator/settings.py
@@ -151,7 +151,7 @@ CORS_ORIGIN_ALLOW_ALL = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticroot')
-STATIC_URL = '/static/'
+STATIC_URL = '/ajirastatic/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 MEDIA_URL = '/media/'

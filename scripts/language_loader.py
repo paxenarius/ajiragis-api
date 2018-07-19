817ffb22838601ea28cc45c5b4e106af71422141
bmarika <mwaura.marika@gmail.com>
2018-07-20 00:04:33 +0300

    Use the Kenyan  language file

--- scripts/language_loader.py
+++ scripts/language_loader.py
@@ -9,28 +9,62 @@ from translation.models import Language
 LOGGER = logging.getLogger(__name__)
 
 
-def extract_names(names):
-    split_name = names.split(';')
-    actual_name = ''.join(split_name[0])
-    alt_names = ''.join(split_name[1:])
-    return actual_name, alt_names
+def extract_alternative_names(row):
+    """
+    Split the alt names by either , or / based on the format of the language.csv file
+    """
+    if not len(row):
+        LOGGER.info('The row is empty =>'.format(
+            row)
+        )
+        return
+
+    alt_names = row[1]
+    if  alt_names.find(',') != -1:
+        alt_names = alt_names.split(',')
+        return ','.join(alt_names)
+
+    elif alt_names.find('/') != -1:
+        alt_names = alt_names.split('/')
+        return ','.join(alt_names)
+    else:
+        LOGGER.warning('The alternative_names column appears to have other characters =>'.format(
+            row)
+    )
+
+    return alt_names
+
 
 def load_languages():
+    """
+    The expected format is of the CSV file
+
+    Row 1:  Language
+         this is the same as the name
+
+    Row 2: Alternative_names
+        A comma or forward slash separated list.
+
+    Row 3:iso_639_2_code
+        A 3 character unique representation of the language.
+
+    """
     language_file_path = os.path.join(settings.BASE_DIR, 'scripts/data/language.csv')
     with open(language_file_path, 'r') as csvfile:
         data = csv.reader(csvfile, delimiter=',')
 
-        # Skip csv header
+        # Skip CSV file header
         next(data)
         for row in data:
             try:
-                Language.objects.get(iso_639_2_code=row[0])
+                Language.objects.get(iso_639_2_code=row[2])
                 LOGGER.info("{} already exists".format(row[0]))
             except Language.DoesNotExist:
-                actual_name, alt_names = extract_names(row[2])
+                alt_names = extract_alternative_names(row)
+
                 Language.objects.create(
-                    name=actual_name,
-                    iso_639_1_code=row[1],
-                    iso_639_2_code=row[0],
+                    name=row[0],
+                    iso_639_1_code=None,
+                    iso_639_2_code=row[2],
                     alternative_names=alt_names,
-                )
+                ) if row else None

709bd4bc43d2c22b021b14fa64918e79864b906b
bmarika <mwaura.marika@gmail.com>
2018-07-26 23:04:27 +0300

    Implement random words on the model and the view and add tests for the same.

--- translation/tests.py
+++ translation/tests.py
@@ -34,3 +34,31 @@ class TestWordFiltering(APITestCase):
         assert response.status_code == 200
         assert response.data != []
         assert len(response.data) == 1
+
+    def test_getting_random_words_params_no_okay(self):
+        language = mommy.make(Language, iso_639_2_code='swa')
+        mommy.make(Word, language=language, part_of_speech='NOUN')
+        url = reverse('world-list-api')
+        response = self.client.get(url, {'random': 'True'})
+        assert response.status_code == 400
+        error = {"language":["Please ensure the language provided exists"]}
+        assert response.data ==  error
+
+    def test_getting_random_words_no_word_in_language(self):
+        language = mommy.make(Language, iso_639_2_code='swa')
+        url = reverse('world-list-api')
+        response = self.client.get(url, {'random': 'True', 'language': language.id})
+        assert response.status_code == 200
+        assert response.data == []
+
+    def test_filter_getting_random_words_okay(self):
+        language = mommy.make(Language, iso_639_2_code='swa')
+        mommy.make(Word,  language=language, part_of_speech='NOUN', _quantity=5)
+        url = reverse('world-list-api')
+        response = self.client.get(url, {'random': 'True', 'language': language.id})
+        assert response.status_code == 200
+        assert len(response.data) == 1
+        response = self.client.get(url, {'random': 'True', 'language': language.id})
+        assert len(response.data) == 1
+        response = self.client.get(url, {'random': 'True', 'language': language.id})
+        assert len(response.data) == 1
--- translation/views.py
+++ translation/views.py
@@ -2,6 +2,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView, ListView, DetailView
+
+from rest_framework.exceptions import ValidationError
 from rest_framework.generics import ListAPIView, ListCreateAPIView, RetrieveAPIView
 from rest_framework.response import Response
 
@@ -68,6 +70,42 @@ class WordApiView(ListCreateAPIView):
     queryset = Word.objects.all()
     serializer_class = WordSerializer
     filter_class = WordFilter
+
+    def validate_language_exists(self, language):
+        try:
+            language =  Language.objects.get(id=language)
+            return True
+        except Language.DoesNotExist:
+            return False
+
+    def validate_query_params_for_getting_random_words(self):
+        random = 'random' in self.request.query_params
+        language = 'language' in self.request.query_params
+
+        if random & language:
+            return True
+        else:
+            return False
+
+    def get_random_word(self):
+        language_id = self.request.query_params.get('language')
+        params_are_valid = self.validate_query_params_for_getting_random_words()
+        language_is_valid = self.validate_language_exists(language_id)
+
+        if params_are_valid & language_is_valid:
+            language_obj =  Language.objects.get(id=language_id)
+            if Word.objects.filter(language=language_obj).count() > 0:
+                return  Word.objects.random(language_obj)
+            else:
+                # return an empty queryset
+                return Word.objects.none();
+        else:
+            raise ValidationError({"language":["Please ensure the language provided exists"]})
+
+    def get_queryset(self):
+        if 'random' in self.request.query_params:
+            return self.get_random_word()
+        return self.queryset
 
 
 class LanguageApiView(ListCreateAPIView):

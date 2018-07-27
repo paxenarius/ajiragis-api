66900e58e8f0c9a723d4a0fdc0be0d62dbe07b8b
bmarika <mwaura.marika@gmail.com>
2018-07-27 04:40:42 +0300

    Implement the dashboard.

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,16 @@
 from django.conf.urls import url
 from django.urls import include, path
+
 from rest_framework_swagger.views import get_swagger_view
-from translation.views import TranslationApiView, WordApiView, LanguageApiView, PaymentApiView, LanguageDetailApiView
+
+from dashboard.views import DashboardAPI
+from translation.views import (
+    TranslationApiView,
+    WordApiView,
+    LanguageApiView,
+    PaymentApiView,
+    LanguageDetailApiView
+)
 
 schema_view = get_swagger_view(title='AjiraGIS API')
 
@@ -11,7 +20,8 @@ urlpatterns = [
     path('words/', WordApiView.as_view(), name='world-list-api'),
     path('languages/', LanguageApiView.as_view(), name='languages-list-api'),
     path('languages/<pk>/', LanguageDetailApiView.as_view()),
-    path('payments/', PaymentApiView .as_view()),
+    path('payments/', PaymentApiView.as_view()),
+    path('dashboard/', DashboardAPI.as_view(), name='dashboard'),
     path('rest-auth/', include('rest_auth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
     url(r'doc/', schema_view)
--- dashboard/views.py
+++ dashboard/views.py
@@ -3,6 +3,11 @@ from django.views.generic import TemplateView
 
 from rest_framework.views import APIView, Response
 
+from translation.models import Translation, Payment
+from translation.serializers import TranslationSerializer, Paymentserializer
+from users.serializers import CustomUserDetailSerializer
+
+
 class Dashboard(TemplateView):
     template_name = 'dashboard/dashboard.html'
 
@@ -10,25 +15,34 @@ class Dashboard(TemplateView):
 class DashboardAPI(APIView):
     """
     For now the dashboard will return the data for the authenticated user until the data for
-    the Admin Users has been authenticated.
+    the Admin Users has been defined.
     """
     def get_work_data(self):
         """Returns the number of translations and data inputs that a user has made"""
-
+        user_data = Translation.objects.filter(user=self.request.user)
+        serialiazer = TranslationSerializer(data=user_data, many=True)
+        if serialiazer.is_valid():
+            return serialiazer.data
         return {}
 
     def get_wallet_data(self):
         """
         Returns the number of Ajira points that someone has gained based on the number of
         translations and contributions
+
+        # TODO Need to factor in historical data what if the points per task change?
         """
+        translation_data = Payment.objects.filter(user=self.request.user).count()
         return {
             "contributions": 0,
-            "translations": 0
+            "translations": translation_data
         }
 
     def get_profile(self):
         """Details of the logged in User"""
+        serialiazer = CustomUserDetailSerializer(data=self.request.user)
+        if serialiazer.is_valid():
+            return serialiazer.data
         return {}
 
     def get_notifications(self):
@@ -37,9 +51,9 @@ class DashboardAPI(APIView):
 
     def get(self, request, *args, **kwargs):
         data = {
-            "my_work":0,
-            "my_wallet": 0,
-            "my_profile": 0,
-            "notifications":0,
+            "my_work":self.get_work_data(),
+            "my_wallet": self.get_wallet_data(),
+            "my_profile": self.get_profile(),
+            "notifications":self.get_notifications(),
         }
         return Response(data=data, status=200)
